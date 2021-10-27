8ef69d5f19383e90bb97d7daacae4f31b04652c1
Alexey Zyukov <aalex64@ukr.net>
2021-10-27 22:20:21 +0300

    revised apiService
    добавлена прокрутка страницы при догрузке изображений; выполнен сброс № страницы в 1 при поиске нового типа изображений

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -6,12 +6,7 @@ import { fetchImages } from './fetch';
 import { openModal } from './modal';
 
 
-
-// console.log(inputField.value)
-// console.log(galleryImagesList);
-
-let isAlertVisible = false;
-
+let isAlertVisible = false; // управляет "видимостью" кнопки догрузки изображений
 let page = 1; // Controls the group number
 let limit = 12; // Controls the quantity of items in the group to display
 let nameOfImage = ""; //select name of the image
@@ -25,24 +20,24 @@ searchForm.addEventListener("submit", (event) => {
     alert('Введите название изображения')
     return
   }
-  if (nameOfImage) {
-    if (nameOfImage === inputField.value) {
-      // console.log(inputField.value); 
-      // console.log(nameOfImage);
-      alert("Поиск изображения с таким названием был выполнен, введите другое название")
-      return
-    }
+
+  if (nameOfImage === inputField.value) {
+    // console.log(inputField.value); 
+    // console.log(nameOfImage);
+    alert("Поиск изображения с таким названием был выполнен, введите другое название")
+    return
   }
-  if (nameOfImage) { //выполненеие поиска изображения
-    if (nameOfImage !== inputField.value) {
-      // console.log(inputField.value, nameOfImage);
-      nameOfImage = inputField.value;
-      // console.log(nameOfImage);
-      clearContent();
-      page = 1;
-      // console.log(nameOfImage);
-    }
+
+  //выполненеие поиска изображения
+  if (nameOfImage !== inputField.value) {
+    // console.log(inputField.value, nameOfImage);
+    nameOfImage = inputField.value;
+    // console.log(nameOfImage);
+    clearContent();
+    page = 1;
+    // console.log(nameOfImage);
   }
+
   else {
     nameOfImage = inputField.value;
     // console.log(nameOfImage)
@@ -65,7 +60,7 @@ searchForm.addEventListener("submit", (event) => {
     .catch((error) => console.log(error));
 });
 
-//===========добавление новых изображений вниз документа===============
+//===========добавление новых изображений вниз страницы===============
 fetchImagesBtnAdditional.addEventListener('click', (event) => {
   event.preventDefault;
   fetchImages()
@@ -76,9 +71,16 @@ fetchImagesBtnAdditional.addEventListener('click', (event) => {
       const totalPages = Math.ceil(images.total / limit); //get quantity of pages in the images'collection
       // console.log(totalPages);
       // console.dir(images.hits); //при обращении к значению объекта hits => Array, св-во length сохраняется
-      renderImages(images.hits); //получние доступа к Массиву изображений в Объекте Json; 
-      //при обращении к значению объекта hits => Array, св - во length сохраняется
-      buttonShowOnPageIncrease(totalPages)
+      renderImages(images.hits); //получение доступа к Массиву изображений в Объекте Json; 
+      //при обращении к значению объекта hits => через Array, св - во length сохраняется
+      buttonShowOnPageIncrease(totalPages);
+
+      //====прокрутка экрана при загрузке новых изображений====
+      const element = document.querySelector('#my-element-selector');
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
     })
     .catch((error) => console.log(error));
 });
@@ -121,42 +123,3 @@ function toggleAlertPopupEndOfItems() {
     isAlertVisible = false;
   }, 2000);
 };
-
-
-
-//=================================================================
-// function markupImages(images) { //формирует разметку карточки
-//     // console.log(images);
-//   if (images.isArray) {
-//    return images.map(({ likes, views, comments, downloads, id }) => {
-//     return `<div class="photo-card">
-//   <img src=${webformatURL} alt=${tags} />
-
-//   <div class="stats">
-//     <p class="stats-item">
-//       <i class="material-icons">thumb_up</i>
-//       ${likes}
-//     </p>
-//     <p class="stats-item">
-//       <i class="material-icons">visibility</i>
-//       ${views}
-//     </p>
-//     <p class="stats-item">
-//       <i class="material-icons">comment</i>
-//       ${comments}
-//     </p>
-//     <p class="stats-item">
-//       <i class="material-icons">image_id</i>
-//       ${id}
-//     </p>
-//     <p class="stats-item">
-//       <i class="material-icons">cloud_download</i>
-//       ${downloads}
-//     </p>
-//   </div>
-// </div>`;
-//     })
-//     .join("");
-//   }
-//   // return "input of .map() is not array";
-// };
